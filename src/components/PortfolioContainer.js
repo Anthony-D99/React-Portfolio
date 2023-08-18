c304e4de2da1ca8b9d8388b8011c897a2c1686fb
Anthony-D99 <anthonydisaf@yahoo.com>
2023-08-18 16:01:44 -0400

    initial commit

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}

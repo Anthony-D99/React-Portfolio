e950b06b47a7197ae450dad22a61892b6445b791
Anthony-D99 <anthonydisaf@yahoo.com>
2023-08-21 18:15:20 -0400

    resume section is now complete.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import {saveAs} from 'file-saver'
-const image_url = 'https://images.resumgo.com/2019/08/DIONA-Free-Resume-Template.png'
+// import { saveAs } from 'file-saver'
+import image_url from '../../assets/images/DIONA-Free-Resume-Template.png'
 
-const download= ()=>{
-  saveAs(image_url, 'DIONA-Free-Resume-Template.png')
-}
+// const download= ()=>{
+//   saveAs(image_url, 'DIONA-Free-Resume-Template.png')
+// }
 
 export default function Resume() {
   return (
     <div className>
       <h1 className='offset-5 '>Resume</h1>
-      <img className='offset-1' onClick={download} src={image_url} alt='resume' />
-        
-      
+      <img className='offset-1' src={image_url} alt='resume' />
+      <a href={image_url} download='DIONA-Free-Resume-Template.png'  >
+        Download Resume
+      </a>
     </div>
   );
 }

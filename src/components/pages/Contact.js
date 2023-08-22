4d8356c17b5fee97d89e687cc54a4761f66fe7fc
Anthony-D99 <anthonydisaf@yahoo.com>
2023-08-22 15:31:38 -0400

    completed form for contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,42 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
- 
+export function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
+export default function Form() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-
-export default function Contact() {
-  const submitEmail = ()=>{
-    const emailHelp = document.getElementById('emailHelp')
-    const userEmail = document.getElementById('exampleInputEmail1');
-    let regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
-    if(userEmail === regex.test(userEmail)){
-      emailHelp.innerHTML = "The Email is valid"
-    }else{
-      emailHelp.innerHTML = "The Email is not a valid email address"
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
     }
-  
-  }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid');
+      return;
+    } if (!name) {
+      setErrorMessage(
+        `Name field can not be empty`
+      );
+      return;
+    }
+    if (!message) {
+      setErrorMessage(
+        `Message field can not be empty`
+      );
+      return;
+    }
+
+
+    setName('');
+    setMessage('');
+    setEmail('');
+    setErrorMessage('')
+  };
+
   return (
+
     <div>
-      <h1 className='offset-5'>Contact Page</h1>
-      <form className='col-6 offset-3'>
-      <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">Please enter a valid Email address</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputName1" class="form-label">name</label>
-    <input type="name" class="form-control" id="exampleInputName1"/>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputMessage1" class="form-label">message</label>
-    <input type="message" class="form-control" id="exampleInputMessage1"/>
-  </div>
-  <button onClick={submitEmail()}>Submit</button>
-      </form>
+      <h1 className='offset-5'>Contact Me</h1>
+
+      <form className="form col-6 offset-3">
+        <div className='mb-3'>
+          <input
+          className='form-control'
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div className='mb-3'>
+
+          <input
+          className='form-control'
+            value={name}
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Name"
+          />
+        </div>
+        <div className='mb-3'>
+          <input
+          className='form-control'
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="message"
+            placeholder="Message"
+          />
+        </div>
+        {errorMessage && (
+        <div>
+          <p className="error-text offset-4">{errorMessage}</p>
+        </div>
+      )}
+        <button type="button" className='btn btn-primary' onClick={handleFormSubmit}>Submit</button>
+      </form> 
+
+      
     </div>
   );
 }
